17ecf31f6c0b6e022d57f54f615f4543fb1c5eb4
lexchristopher03 <lexchristopher03@icloud.com>
2024-10-29 17:14:33 -0600

    updated

--- ica/ica10.js
+++ ica/ica10.js
@@ -5,7 +5,7 @@ console.log(document.getElementById("my_head"));
 document.getElementById("my_head").addEventListener("click", function(e){
     alert("clicked the h1");
     // change paragraph when clicked on it
-    document.getElementById("paragraph").innerHTML = "this is the new text";
+    document.getElementById("paragraph").innerHTML = "this is Geralds GF Gwen, say Hi to her!";
     document.getElementById("paragraph").style.color= "blue";
     // change attributes of an element- change image when clicked
     document.getElementById("image").src="giraffeFun.jpeg";
@@ -13,7 +13,7 @@ document.getElementById("my_head").addEventListener("click", function(e){
 })
 
 document.getElementById("small_description").addEventListener("mouseenter", function(e){
-    document.getElementById("second_paragraph").innerHTML = "this is new text that was hidden";
+    document.getElementById("second_paragraph").innerHTML = "this is Gerald the Giraffe, say HI!";
     document.getElementById("second_paragraph").style.color= "red";
 
 })
